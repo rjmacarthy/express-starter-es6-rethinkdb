ff91f3492ff6dd326c60328edd765341c6bab007
rjmacarthy <richardmacarthy@hotmail.com>
2016-02-22 21:26:04 +0000

    Remove

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,6 @@ exports.config = {
     rethinkPort : 28015,
     routes: './src/routes/**/*.js',
     models: './src/models/**/*.js',
-    db: 'mongodb://localhost/express-api-es6',
     title : 'express api boilerplate es6',
     globFiles: function(location) {
         var files = glob.sync(location);
