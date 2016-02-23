054cf12908c19e2f0e91027f003935322faaf916
Richard Macarthy <richard.macarthy@dezrez.com>
2016-02-23 10:26:51 +0000

    Another thinky fix

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,12 @@
 import thinky from 'thinky';
 import { config } from '../config/config';
 
-var instance = thinky({
+const thnk = thinky({
     host: process.env.RDB_HOST || 'localhost',
     port: parseInt(process.env.RDB_PORT || config.rethinkPort),
     db: process.env.RDB_DB || 'Test'
 });
 
-const thnk = thinky();
 const r = thnk.r;
 
-export { instance, thnk, r };
+export { thnk, r };
